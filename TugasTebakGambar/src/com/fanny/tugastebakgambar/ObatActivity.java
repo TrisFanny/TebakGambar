7e88ae5bbd525bda01c6db10b168429306691fab
TrisFanny <Trisfanny@ymail.com>
2014-12-15 14:30:58 +0700

    First Commit

---
+++ TugasTebakGambar/src/com/fanny/tugastebakgambar/ObatActivity.java
@@ -0,0 +1,51 @@
+package com.fanny.tugastebakgambar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ObatActivity extends Activity {
+
+	EditText JawabanTxt;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_obat);
+		
+		JawabanTxt = (EditText)findViewById(R.id.editText1);
+		JawabanTxt.setFilters(new InputFilter[] {new InputFilter.AllCaps()});
+	}
+	
+	public void cekjawaban(View y){
+    	String jawabanBenar = "OBAT NYAMUK";
+    	String jawabanUser = JawabanTxt.getText().toString();
+    	if(jawabanUser.equals(jawabanBenar)){
+    		//pesan toast
+    		Toast.makeText(getApplicationContext(), "JAWABAN TEPAT", Toast.LENGTH_LONG).show();
+    		Intent i = new Intent(getApplicationContext(),PerangkapActivity.class);
+    		startActivity(i);
+    	}
+    	else{
+    		//pesan toast
+    		Toast.makeText(getApplicationContext(),"JAWABAN MASIH SALAH", Toast.LENGTH_LONG).show();
+    	}
+    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
